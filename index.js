225895979ebeab58d298b86c717d19cc679312ea
Endercat <84635059+endercat126@users.noreply.github.com>
2022-04-26 21:05:16 -0400

    something

---
+++ index.js
@@ -0,0 +1,9 @@
+function dropdownMenu() {
+    var x = document.getElementById("navbar");
+    
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
